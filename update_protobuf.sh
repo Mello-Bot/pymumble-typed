7a6def7301c5e11ba856296f740d7bdd73db6918
Nicola <nx9889@gmail.com>
2024-12-14 10:44:48 +0100

    build: updated script to generate protobuf src

--- update_protobuf.sh
+++ update_protobuf.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-
-protoc --proto_path=./protobuf --python_out=./pymumble_typed/protobuf protobuf/Mumble.proto protobuf/MumbleUDP.proto --mypy_out=./pymumble_typed/protobuf
+python -m grpc_tools.protoc -I=./protobuf --python_out=./src/pymumble_typed/protobuf --pyi_out=./src/pymumble_typed/protobuf protobuf/*.proto
