925b3ee93079813a938104cc09e29e074bf831f0
Nicola C. <nx9889@gmail.com>
2025-04-14 10:57:41 +0200

    chore: increased version

--- src/pymumble_typed/constants.py
+++ src/pymumble_typed/constants.py
@@ -1,4 +1,4 @@
-VERSION = (4, 0, 5)  # This library version
+VERSION = (4, 0, 6)  # This library version
 PROTOCOL_VERSION = (1, 5, 0)  # Mumble protocol version
 OS = f"PyMumble"  # Reported OS information
 OS_VERSION = f"{VERSION}"  # Reported OS version
